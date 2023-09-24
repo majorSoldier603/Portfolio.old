1e684296b40eae8d475264d6394fe09f82ea5d86
Schmax603 <Stark.m.2004@gmail.com>
2023-09-24 21:10:54 +0200

    css

--- src/app/arrow-scroll/arrow-scroll.component.ts
+++ src/app/arrow-scroll/arrow-scroll.component.ts
@@ -10,5 +10,16 @@ export class ArrowScrollComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				console.log(entry);
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show');
+				}
+			})
+		});
+
+		const hiddenElements = document.querySelectorAll('.hidden');
+		hiddenElements.forEach((el) => observer.observe(el));
 	}
 }
