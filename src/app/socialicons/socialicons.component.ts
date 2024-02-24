e271f499c4e2b683c5e6fa636ef445616e8c6955
schmax <stark.m.2004@gmail.com>
2024-02-24 20:43:28 +0100

    fry fix

---
+++ src/app/socialicons/socialicons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-socialicons',
+  templateUrl: './socialicons.component.html',
+  styleUrls: ['./socialicons.component.scss']
+})
+export class SocialiconsComponent {
+
+}
