13b9dca483fde2a44108885a393c2abfd5d0c653
Schmax603 <Stark.m.2004@gmail.com>
2024-03-23 23:36:56 +0100

    lol

--- src/app/on-scroll-arrow/on-scroll-arrow.component.ts
+++ src/app/on-scroll-arrow/on-scroll-arrow.component.ts
@@ -1,27 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-on-scroll-arrow',
   templateUrl: './on-scroll-arrow.component.html',
   styleUrls: ['./on-scroll-arrow.component.scss']
 })
-export class OnScrollArrowComponent implements OnInit {
+export class OnScrollArrowComponent {
+items: any;
 	constructor() { }
 	
 	@Input() arrowId = 0;
 
-	ngOnInit() {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					setTimeout(() => {
-						entry.target.classList.add('show');
-					}, 10000);
-				}
-			})
-		});
+	isHovered: boolean = false;
 
-		const hiddenElements = document.querySelectorAll('.hidden');
-		hiddenElements.forEach((el) => observer.observe(el));
+	toggleHover(value: boolean): void {
+	  	this.isHovered = value;
 	}
 }
