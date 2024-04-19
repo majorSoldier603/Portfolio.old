9ce1b966933ce0a48d70ef04fb9cc3ae4005a00b
Major Soldier <majorsoldier603@proton.me>
2024-04-20 00:19:05 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
-import { Component, HostListener } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef, HostListener } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 	title = 'Portfolio';
 
 	buttonText: string = 'Send message';
 	textemail: string = '© Maximilian Stark 2024';
-	items: any;
-	
+
+	constructor() {
+		this.scrollArrows = new QueryList<ElementRef>();
+	}
+
+	@ViewChildren('scrollArrow', { read: ElementRef }) scrollArrows: QueryList<ElementRef>;
+
 	@HostListener('window:resize', ['$event'])
 	onResize(event: any) {
-	  this.checkViewportWidth();
+		this.checkViewportWidth();
 	}
-  
+
+	items = Array(1).fill(0);
+
+	ngAfterViewInit() {
+		this.scrollArrows.forEach((scrollArrow, index) => {
+			scrollArrow.nativeElement.id = 'arrow' + index;
+		});
+	}
+
 	checkViewportWidth() {
-	  this.buttonText = window.innerWidth < 900 ? 'Say hello ;)' : 'Send message';
-	  this.textemail = window.innerWidth < 900 ? 'me@maximilian-stark.dev' : '© Maximilian Stark 2024';
+		this.buttonText = window.innerWidth < 900 ? 'Say hello ;)' : 'Send message';
+		this.textemail = window.innerWidth < 900 ? 'me@maximilian-stark.dev' : '© Maximilian Stark 2024';
 	}
 }
