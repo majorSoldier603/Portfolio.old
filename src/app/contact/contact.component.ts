3ea83363419330dd3b4f9f62aec9cf23c8fa4dac
Major Soldier <majorsoldier603@proton.me>
2024-06-19 21:05:13 +0200

    lol

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,84 +2,64 @@ import { Component, HostListener, OnInit, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
-	selector: 'app-contact',
-	templateUrl: './contact.component.html',
-	styleUrls: ['./contact.component.scss']
+		selector: 'app-contact',
+		templateUrl: './contact.component.html',
+		styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-	constructor() { }
+		constructor() { }
 
-	ngOnInit(): void {
-		this.checkViewportWidth();
-	}
+		ngOnInit(): void {
+			this.checkViewportWidth();
+		}
 
-	buttonText: string = 'Send message';
+		buttonText: string = 'Send message';
 
-	@HostListener('window:resize', ['$event'])
-  
-	checkViewportWidth() {
-	  	this.buttonText = window.innerWidth < 900 ? 'Say hello ;)' : 'Send message';
-	}
+		@HostListener('window:resize', ['$event'])
+	
+		checkViewportWidth() {
+			this.buttonText = window.innerWidth < 900 ? 'Say hello ;)' : 'Send message';
+		}
 
-	contactFormData = {
-		name: '',
-		email: '',
-		message: ''
-	}
+		contactData = {
+			name: '',
+			email: '',
+			message: ''
+		}
 
+		mailTest = false;
 
-	mailTest = true;
+		http = inject(HttpClient);
 
-	http = inject(HttpClient);
-
-	post = {
-	  endPoint: '/sendMail.php',
-	  body: (payload: any) => JSON.stringify(payload),
-	  options: {
-		headers: {
-		  'Content-Type': 'text/plain',
-		  responseType: 'text',
-		},
-	  },
-	};
-
-	posttest = {
-		endPoint: '/testmail.php',
-		body: (payload: any) => JSON.stringify(payload),
-		options: {
-		  headers: {
-			'Content-Type': 'text/plain',
-			responseType: 'text',
-		  },
-		},
-	  };
-  
-	onSubmit(ngForm: NgForm) {
-	  if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-		this.http.post(this.post.endPoint, this.post.body(this.contactFormData))
-		  .subscribe({
-			next: (response) => {
-				console.log(response);
-			  ngForm.resetForm();
+		post = {
+			endPoint: 'https://maximilian-stark.dev/sendMail.php',
+			body: (payload: any) => JSON.stringify(payload),
+			options: {
+				headers: {
+					'Content-Type': 'text/plain',
+					responseType: 'text',
+				},
 			},
-			error: (error) => {
-			  console.error(error);
-			},
-			complete: () => console.info('send post complete'),
-		  });
-	  } else if (ngForm.submitted && this.mailTest) {
-		this.http.post(this.posttest.endPoint, this.post.body(this.contactFormData))
-		  .subscribe({
-			next: (response) => {
-				console.log(response);
-			  ngForm.resetForm();
-			},
-			error: (error) => {
-			  console.error(error);
-			},
-			complete: () => console.info('send testpost complete'),
-		});
-	  	}
+		};
+	
+		onSubmit(ngForm: NgForm) {
+		if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+			this.http.post(this.post.endPoint, this.post.body(this.contactData))
+				.subscribe({
+					next: (response) => {
+
+						ngForm.resetForm();
+					},
+					error: (error) => {
+						console.error(error);
+					},
+					complete: () => console.info('send post complete'),
+				});
+		} else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+			ngForm.resetForm();
+		}
 	}
 }
